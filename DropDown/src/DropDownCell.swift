bed6ae154dd5c3da94222aa41958c1a52af3ab3c
Thompson Sanjoto <tsanjoto@gmail.com>
2021-06-01 21:53:28 -0700

    Update DropDownCell.swift

--- DropDown/src/DropDownCell.swift
+++ DropDown/src/DropDownCell.swift
@@ -11,68 +11,83 @@
 import UIKit
 
 open class DropDownCell: UITableViewCell {
-		
-	//UI
-	@IBOutlet open weak var optionLabel: UILabel!
-	
-	var selectedBackgroundColor: UIColor?
+        
+    //UI
+    @IBOutlet open weak var optionLabel: UILabel!
+    
+    var selectedBackgroundColor: UIColor?
     var highlightTextColor: UIColor?
     var normalTextColor: UIColor?
 
 }
 
+open class DropDownCustomCell: DropDownCell {
+    var strongOptionLabel = UILabel(frame: .zero)
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        optionLabel = strongOptionLabel
+        contentView.addSubview(strongOptionLabel)
+    }
+}
+
 //MARK: - UI
 
 extension DropDownCell {
-	
-	override open func awakeFromNib() {
-		super.awakeFromNib()
-		
-		backgroundColor = .clear
-	}
-	
-	override open var isSelected: Bool {
-		willSet {
-			setSelected(newValue, animated: false)
-		}
-	}
-	
-	override open var isHighlighted: Bool {
-		willSet {
-			setSelected(newValue, animated: false)
-		}
-	}
-	
-	override open func setHighlighted(_ highlighted: Bool, animated: Bool) {
-		setSelected(highlighted, animated: animated)
-	}
-	
-	override open func setSelected(_ selected: Bool, animated: Bool) {
-		let executeSelection: () -> Void = { [weak self] in
-			guard let `self` = self else { return }
+    
+    override open func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = .clear
+    }
+    
+    override open var isSelected: Bool {
+        willSet {
+            setSelected(newValue, animated: false)
+        }
+    }
+    
+    override open var isHighlighted: Bool {
+        willSet {
+            setSelected(newValue, animated: false)
+        }
+    }
+    
+    override open func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        setSelected(highlighted, animated: animated)
+    }
+    
+    override open func setSelected(_ selected: Bool, animated: Bool) {
+        let executeSelection: () -> Void = { [weak self] in
+            guard let `self` = self else { return }
 
-			if let selectedBackgroundColor = self.selectedBackgroundColor {
-				if selected {
-					self.backgroundColor = selectedBackgroundColor
+            if let selectedBackgroundColor = self.selectedBackgroundColor {
+                if selected {
+                    self.backgroundColor = selectedBackgroundColor
                     self.optionLabel.textColor = self.highlightTextColor
-				} else {
-					self.backgroundColor = .clear
+                } else {
+                    self.backgroundColor = .clear
                     self.optionLabel.textColor = self.normalTextColor
-				}
-			}
-		}
-		
-		if animated {
-			UIView.animate(withDuration: 0.3, animations: {
-				executeSelection()
-			})
-		} else {
-			executeSelection()
-		}
+                }
+            }
+        }
+        
+        if animated {
+            UIView.animate(withDuration: 0.3, animations: {
+                executeSelection()
+            })
+        } else {
+            executeSelection()
+        }
 
-		accessibilityTraits = selected ? .selected : .none
-	}
-	
+        accessibilityTraits = selected ? .selected : .none
+    }
+    
 }
+
 
 #endif
